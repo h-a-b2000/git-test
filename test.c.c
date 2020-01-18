87353dee7dae54e24a27c0396ee219a0b0cb9135
h-a-b2000 <59295075+h-a-b2000@users.noreply.github.com>
2020-01-19 00:58:26 +0330

    Update test.c.c

--- test.c.c
+++ test.c.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-/*COORD coord ={0,0};
+char matrix[7][12];
+int score=0;
+COORD coord ={0,0};
 void gotoxy(int x,int y)
 {
 	coord.X=x;
 	coord.Y=y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
 }
-int main() {
+/*int main() {
 	gotoxy(3,2);
 	printf("!");
 }*/
@@ -17,8 +19,9 @@ typedef struct{
 	int x;
 	int y;
 }location;
+location pakman;
 
-void print_matrix(char matrix[][12])
+void print_matrix()
 {
 	for(int i=0;i<7;i++)
 	{
@@ -28,7 +31,7 @@ void print_matrix(char matrix[][12])
 	}
 }
 
-location make_matrix(char matrix[][12])
+location make_matrix()
 {
 	char c;
 	location pakman;
@@ -72,11 +75,123 @@ location make_matrix(char matrix[][12])
 		matrix[k][i]=176;
 	return pakman;
 }
-void move
+int check_food(){
+	for(int i=0;i<7;i++)
+		for(int j=0;j<12;j++)
+			if(matrix[i][j]=='*')
+				return 0;
+	return 1;
+}
+void move(){
+	char c1,c2;
+	while(1)
+	{
+	c1=getch();
+	if(c1=='-')
+	{
+		c2=getch();
+		if(c2=='1')
+		return;
+	}
+	if(c1==-32)
+	{
+	c2=getch();
+	switch(c2){
+		case 72:
+			up();
+			break;
+		case 75:
+			left();
+			break;
+		case 77:
+			right();
+			break;
+		case 80:
+			down();
+			break;
+	}
+	}
+	if(check_food())
+	return;
+	}
+}
+//////////////////////////////////
+void left(){
+	if(matrix[pakman.y][(pakman.x)-1]!=(-80))
+	{
+	gotoxy(pakman.x,pakman.y);
+	printf(" ");
+	matrix[pakman.y][pakman.x]=' ';
+	pakman.x--;
+	if(matrix[pakman.y][pakman.x]=='*')
+	score++;
+	gotoxy(pakman.x,pakman.y);
+	printf("O");
+	matrix[pakman.y][pakman.x]='O';
+	}
+	gotoxy(0,10);
+	printf("%d",score);
+}
+//////////////////////////////////
+void right(){
+	if(matrix[pakman.y][(pakman.x)+1]!=(-80))
+	{
+	gotoxy(pakman.x,pakman.y);
+	printf(" ");
+	matrix[pakman.y][pakman.x]=' ';
+	pakman.x++;
+	if(matrix[pakman.y][pakman.x]=='*')
+	score++;
+	gotoxy(pakman.x,pakman.y);
+	printf("O");
+	matrix[pakman.y][pakman.x]='O';
+	}
+	gotoxy(0,10);
+	printf("%d",score);
+}
+/////////////////////////////////
+void down(){
+	if(matrix[pakman.y+1][(pakman.x)]!=(-80))
+	{
+	gotoxy(pakman.x,pakman.y);
+	printf(" ");
+	matrix[pakman.y][pakman.x]=' ';
+	pakman.y++;
+	if(matrix[pakman.y][pakman.x]=='*')
+	score++;
+	gotoxy(pakman.x,pakman.y);
+	printf("O");
+	matrix[pakman.y][pakman.x]='O';
+	}
+	gotoxy(0,10);
+	printf("%d",score);
+}
+/////////////////////////////////
+void up(){
+	if(matrix[pakman.y-1][(pakman.x)]!=(-80))
+	{
+	gotoxy(pakman.x,pakman.y);
+	printf(" ");
+	matrix[pakman.y][pakman.x]=' ';
+	pakman.y--;
+	if(matrix[pakman.y][pakman.x]=='*')
+	score++;
+	gotoxy(pakman.x,pakman.y);
+	printf("O");
+	matrix[pakman.y][pakman.x]='O';
+	}
+	gotoxy(0,10);
+	printf("%d",score);
+}
+/////////////////////////////////
 main()
 {
-	char matrix[7][12];
-	location pakman=make_matrix(matrix);
+	pakman=make_matrix(matrix);
 	print_matrix(matrix);
-	printf("%d %d",pakman.x,pakman.y);
+	move();
+	gotoxy(0,10);
+	printf("%d",score);
+	printf("\nend of game!");
+	printf("\npress any key to exit");
+	getch();
 }
